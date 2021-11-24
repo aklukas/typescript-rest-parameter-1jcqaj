5745d43f395d6055dc9480a7ae337108adccd600
aklukas <goldenshore8@gmail.com>
2021-11-24 14:06:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-rest-parameter-1jcqaj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-rest-parameter-1jcqaj)
---
+++ index.ts
@@ -0,0 +1,8 @@
+function x(...a) {
+  for (let i of a) {
+    if (i > 5) return true;
+  }
+  return false;
+}
+// NOTE: Have=ing no idea how many numbers are going to be in the source array or what they are. You just need to know that at least one of them is over 5.
+console.log(x(5, 25, 4));
